cd7db583e3ea5066b525f214ff94a09520d8af2f
Frank Bouwens <frankkie12345@gmail.com>
2015-08-29 12:35:08 +0200

    Fixed ACRA

--- app/src/main/java/nl/frankkie/bronydays2015/MyApplication.java
+++ app/src/main/java/nl/frankkie/bronydays2015/MyApplication.java
@@ -10,7 +10,7 @@ import org.acra.annotation.ReportsCrashes;
  */
 @ReportsCrashes(
         formKey = "", // This is required for backward compatibility but not used
-        formUri = "http://wofje.8s.nl:5984/acra-hwcon/_design/acra-storage/_update/report", //due to a bug in ACRA, url has to be set now, cannot be done later.
+        formUri = "http://wofje.8s.nl:5984/acra-bronydays2015/_design/acra-storage/_update/report", //due to a bug in ACRA, url has to be set now, cannot be done later.
         reportType = org.acra.sender.HttpSender.Type.JSON,
         httpMethod = org.acra.sender.HttpSender.Method.PUT,
         formUriBasicAuthLogin = "", //SECRET to be set later
